bef12ad0987abc279b732ef058924765fb499e70
littleboy1442 <51123828+littleboy1442@users.noreply.github.com>
2025-01-22 11:57:07 -0500

    Change to reflect new CMA fields

--- CmaFileWatcherService/CmaFileWatcherService.cs
+++ CmaFileWatcherService/CmaFileWatcherService.cs
@@ -1,9 +1,12 @@
 ﻿using Dapper;
+using IniParser;
+using IniParser.Model;
 using Microsoft.Data.SqlClient;
 using Syncfusion.XlsIO;
 using System;
 using System.IO;
 using System.ServiceProcess;
+
 
 namespace CmaFileWatcherService
 {
@@ -14,19 +17,43 @@ namespace CmaFileWatcherService
     public partial class CmaFileWatcherService : ServiceBase
     {
         private FileSystemWatcher _fileWatcher;
+        private string _folderPath;
 
         public CmaFileWatcherService()
         {
+            //InitializeComponent();
+            // _fileWatcher = new FileSystemWatcher();
+
             InitializeComponent();
-            _fileWatcher = new FileSystemWatcher();
+            LoadConfiguration();
+            InitializeWatcher();
         }
+        private void LoadConfiguration()
+        {
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("config.ini");
+            _folderPath = data["Settings"]["WatchFolder"];
+        }
+
+        private void InitializeWatcher()
+        {
+            _fileWatcher = new FileSystemWatcher
+            {
+                Path = _folderPath,
+                Filter = "*.xlsx",
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite
+            };
+
+            _fileWatcher.Created += OnNewExcelFile;
+        }
+
 
         protected override void OnStart(string[] args)
         {
-            _fileWatcher.Path = @"C:\CMAInbound"; // Folder to watch
-            _fileWatcher.Filter = "*.xlsx"; // Watch for Excel files
-            _fileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
-            _fileWatcher.Created += OnNewExcelFile;
+            // _fileWatcher.Path = @"C:\CMAInbound"; // Folder to watch
+            //  _fileWatcher.Filter = "*.xlsx"; // Watch for Excel files
+            // _fileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+            //  _fileWatcher.Created += OnNewExcelFile;
             _fileWatcher.EnableRaisingEvents = true;
 
             WriteLog("Service started and folder watching enabled.");
@@ -65,19 +92,20 @@ namespace CmaFileWatcherService
                     IWorksheet worksheet = workbook.Worksheets["CMA Template"];
 
                     // Extract data (example: Customer Name from cell A1)
-                    string customerNumber = worksheet.Range["B3"].DisplayText;
-                    string corpNumber = worksheet.Range["B2"].DisplayText;
-                    string customerName = worksheet.Range["B1"].DisplayText;
+                    string customerNumber = worksheet.Range["B7"].DisplayText;
+                    string corpNumber = worksheet.Range["B6"].DisplayText;
+                    string customerName = worksheet.Range["B5"].DisplayText;
                     customerNumber = !string.IsNullOrEmpty(corpNumber) ? corpNumber : customerNumber;
                     int corpFlag = !string.IsNullOrEmpty(corpNumber) ? 1 : 0;
-                    string buyingGroup = worksheet.Range["K3"].DisplayText;
-                    string SubmittedBy = worksheet.Range["M3"].Text;
+                    string buyingGroup = worksheet.Range["K7"].DisplayText;
+                    string SubmittedBy = worksheet.Range["M7"].Text;
                     DateTime startDate = DateTime.Now;
                     DateTime endDate = DateTime.Now;
-                    string promoTermsText = worksheet.Range["J129"].DisplayText;
-                    string promoFreightTermsText = worksheet.Range["J130"].DisplayText;
-                    string promoFreightMinimumsText = worksheet.Range["J131"].DisplayText;
-                    string PcfTypeText = worksheet.Range["J132"].DisplayText;
+                    string promoTermsText = worksheet.Range["F2"].DisplayText;
+                    string promoFreightTermsText = worksheet.Range["C2"].DisplayText;
+                    string promoFreightMinimumsText = worksheet.Range["D2"].DisplayText;
+                    string PcfTypeText = worksheet.Range["B2"].DisplayText;
+                    string promoFreightTermsOtherAmtText = worksheet.Range["D3"].DisplayText;
 
 
 
@@ -89,7 +117,7 @@ namespace CmaFileWatcherService
                     string status = "N";   // New
 
 
-                    object cellValue = worksheet.Range["P1"].Value;
+                    object cellValue = worksheet.Range["P5"].Value;
                     if (cellValue != null && DateTime.TryParse(cellValue.ToString(), out DateTime startDateTime))
                     {
                         // Successfully parsed the date
@@ -104,7 +132,7 @@ namespace CmaFileWatcherService
                         WriteLog("Unable to convert Start date");
                     }
 
-                    object cellValue2 = worksheet.Range["P3"].Value;
+                    object cellValue2 = worksheet.Range["P7"].Value;
                     if (cellValue != null && DateTime.TryParse(cellValue2.ToString(), out DateTime endDateTime))
                     {
                         // Successfully parsed the date
@@ -151,7 +179,7 @@ namespace CmaFileWatcherService
                                                 });*/
 
 
-                        int currentRow = 6; // Start from row 6
+                        int currentRow = 10; // Start from row 6
                         while (!string.IsNullOrEmpty(worksheet.Range["B" + currentRow].Text))
                         {
                             // Extract Detail Information
@@ -175,9 +203,9 @@ namespace CmaFileWatcherService
                             // Perform the combined insert
                             string combinedQuery = @"
             INSERT INTO Chap_CmaItems 
-            (Cust_name, Cust_num, CMA_Sequence, BuyingGroup, StartDate, EndDate, SubmittedBy, Site, Corp_flag, CmaFilename, Status, Item, Description, SellPrice, PromoTermsText, PromoFreightTermsText, PromoFreightMinimumText, PcfTypeText)
+            (Cust_name, Cust_num, CMA_Sequence, BuyingGroup, StartDate, EndDate, SubmittedBy, Site, Corp_flag, CmaFilename, Status, Item, Description, SellPrice, PromoTermsText, PromoFreightTermsText, PromoFreightMinimumText, PcfTypeText, PromoFreightMinimumsOtherText)
             VALUES 
-            (@Cust_name, @Cust_num, @CMA_Sequence, @BuyingGroup, @StartDate, @EndDate, @SubmittedBy, @Site, @Corp_flag, @CmaFilename, @Status, @Item, @Description, @SellPrice, @PromoTermsText, @PromoFreightTermsText, @PromoFreightMinimumsText, @PcfTypeText)";
+            (@Cust_name, @Cust_num, @CMA_Sequence, @BuyingGroup, @StartDate, @EndDate, @SubmittedBy, @Site, @Corp_flag, @CmaFilename, @Status, @Item, @Description, @SellPrice, @PromoTermsText, @PromoFreightTermsText, @PromoFreightMinimumsText, @PcfTypeText, @PromoFreightMinimumsText)";
 
                             connection.Execute(combinedQuery, new
                             {
@@ -198,6 +226,7 @@ namespace CmaFileWatcherService
                                 PromoTermsText = promoTermsText,
                                 PromoFreightTermsText = promoFreightTermsText,
                                 PromoFreightMinimumsText = promoFreightMinimumsText,
+                                PromoFreightMinimumsOtherText = promoFreightTermsOtherAmtText,
                                 PcfTypeText = PcfTypeText
                             });
 
